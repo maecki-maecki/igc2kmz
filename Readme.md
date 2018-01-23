225260392e09eae5bdad0a940850bd7a2d2f8e2f
Stefan Märkle <stefan.maerkle@netpioneer.de>
2018-01-23 09:54:44 +0100

    --help and --version added if called with only one filename, derives output filename from input

--- Readme.md
+++ Readme.md
@@ -12,10 +12,11 @@ I wanted to view the climbrates of my paragliding flights in google earth but fo
 
 ## Execute
 
-java -jar igc2kml-1.0-SNAPSHOT-jar-with-dependencies.jar myinputfile.igc myoutputfile.kmz
+java -jar igc2kml.jar myinputfile.igc myoutputfile.kmz
 
 This reads in myinputfile.igc in igc format and builds an output file myoutputfile.kmz in kmz format suitable for google earth.
 
+It also can be called with just on feilneame. It then takes that filename, strips extension and adds kmz extension to use as output filename.
 ## Logic
 
 - All times are UTC, nothing converted.
@@ -25,8 +26,10 @@ This reads in myinputfile.igc in igc format and builds an output file myoutputfi
 - Altitude info is taken from barometric pressure but corrected with the help of the gps altitude.
 - Barometric altitude can differ significantly from Gps altitude due to weather changes (even during the flight, e.g. for long xc flights) 
 - Barometric altitude has a finer resolution than Gps altitude.
-- Thus I take the barometric altitude for display but do a correction
-- The correction is a linear interpolation of the difference between GPS and barometric altitude at takeoff and at landing.
+- Thus I take the barometric altitude for display but it needs correction
+- The solution in avionics is to either use a reference pressure ('QNH') or at least ust the real altitude from a map of the startpoint as reference.
+- I do not have QNHs for my flights and I do not want to need an internet connection for map reference
+- So the correction I do is a linear interpolation of the difference between GPS and barometric altitude at takeoff and at landing.
 
 ## Copyrights and Disclaimer
 - The software is (c) by Stefan Märkle
--- src/main/java/de/abgruen/igc2kml/Main.java
+++ src/main/java/de/abgruen/igc2kml/Main.java
@@ -6,8 +6,46 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String [ ] args) throws IOException, TemplateException {
-        IGCReader reader = new IGCReader(args[0],args[1]);
+        // --version
+        if (args[0].toLowerCase().equals("--version")){
+            System.out.println( "igc2kml Version: " + Main.class.getPackage().getImplementationVersion() );
+            System.out.println( "(c) Stefan Märkle");
+            System.exit(0);
+        }
+
+        // --help
+        if (args[0].toLowerCase().equals("--help")){
+            System.out.println( "igc2kml Version:" + Main.class.getPackage().getImplementationVersion() );
+            System.out.println( "(c) Stefan Märkle");
+            System.out.println( "");
+            System.out.println( "usage: java -jar igc2kml.jar [--version] [--help] INPUTFILENAME [OUTPUTFILENAME]");
+            System.out.println( "");
+            System.out.println( "--version     : show version and exit");
+            System.out.println( "--help        : show this help and exit");
+            System.out.println( "INPUTFILENAME : filename of a valid igc file to be processed");
+            System.out.println( "OUTPUTFILENAME: filename of the kmz file to be generated. If omitted filename will be derived from input filename");
+            System.exit(0);
+        }
+
+        String outputFilename;
+        if (args.length<2){
+            // just inputfilename
+            outputFilename = generateOutputFilename(args[0]);
+        } else {
+            // inputfilename and outputfilename
+            outputFilename=args[1];
+        }
+        IGCReader reader = new IGCReader(args[0],outputFilename);
         reader.parse();
         reader.convert();
+    }
+
+    private static String generateOutputFilename(String inputFilename){
+        String body = inputFilename;
+        int extensionStart = body.lastIndexOf(".");
+        if (extensionStart>0) {
+            body = body.substring(0, extensionStart);
+        }
+        return body+".kmz";
     }
 }
